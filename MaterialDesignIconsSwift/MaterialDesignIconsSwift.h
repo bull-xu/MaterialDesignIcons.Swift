1e0009aa995ab1d09e7d8058511c4df1ff29f1c0
Bull Xu <Bull@bulls-macbook-pro.local>
2016-09-08 15:13:43 +1000

    Initial Commit

---
+++ MaterialDesignIconsSwift/MaterialDesignIconsSwift.h
@@ -0,0 +1,19 @@
+//
+//  MaterialDesignIconsSwift.h
+//  MaterialDesignIconsSwift
+//
+//  Created by Bull Xu on 8/09/2016.
+//  Copyright © 2016 Stride Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MaterialDesignIconsSwift.
+FOUNDATION_EXPORT double MaterialDesignIconsSwiftVersionNumber;
+
+//! Project version string for MaterialDesignIconsSwift.
+FOUNDATION_EXPORT const unsigned char MaterialDesignIconsSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MaterialDesignIconsSwift/PublicHeader.h>
+
+
